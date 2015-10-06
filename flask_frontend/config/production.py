2fcf0a99bd75f3e71b22638a4cb0a67353a9cac3
Valian <jakub.skalecki@gmail.com>
2015-10-06 21:02:01 +0200

    changed production config environment variable names

--- flask_frontend/config/production.py
+++ flask_frontend/config/production.py
@@ -6,9 +6,9 @@ import os
 from flask_frontend.config import keys
 
 ProductionConfig = {
-    keys.BACKEND_URL: 'http://{address}:3000'.format(address=os.environ["CORE_PORT_3000_TCP_ADDR"]),
-    keys.BACKEND_LOGIN: os.environ["CORE_ENV_CORE_LOGIN"],
-    keys.BACKEND_PASS: os.environ["CORE_ENV_CORE_PASSWORD"],
+    keys.BACKEND_URL: 'http://{address}:3000'.format(address=os.environ["DOCKER_HOST_IP"]),
+    keys.BACKEND_LOGIN: os.environ["CORE_LOGIN"],
+    keys.BACKEND_PASS: os.environ["CORE_PASSWORD"],
 
     keys.CLOUDINARY_CLOUD_NAME: os.environ["CLOUDINARY_CLOUD_NAME"],
     keys.CLOUDINARY_SECRET: os.environ["CLOUDINARY_SECRET"],
